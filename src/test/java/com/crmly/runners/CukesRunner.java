bd7ba7d788e1a37293eeec4546fef249bd4e91d5
seyda603 <selcanseydaaydin@gmail.com>
2021-12-09 16:26:56 +0300

    selcan last3

--- src/test/java/com/crmly/runners/CukesRunner.java
+++ src/test/java/com/crmly/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/crmly/step_definitions",
         dryRun = false,
-        tags = ""
+        tags = "@selcan"
 )
 public class CukesRunner {
 
