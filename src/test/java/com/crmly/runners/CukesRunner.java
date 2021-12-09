7cd3bc271974b19a494307a1330a404f9cea0cb6
Selda <seldahas.sh@gmail.com>
2021-12-09 14:53:52 +0300

    Last version

--- src/test/java/com/crmly/runners/CukesRunner.java
+++ src/test/java/com/crmly/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/crmly/step_definitions",
         dryRun = false,
-        tags = "@osman"
+        tags = "@filter"
 )
 public class CukesRunner {
 
